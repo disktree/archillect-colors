111a10796282c1a4f22a9302fe94d6f567223283
tong <tong@disktree.net>
2018-11-14 09:47:10 +0100

    Split array to make eval happy

--- src/App.hx
+++ src/App.hx
@@ -9,51 +9,68 @@ import js.html.CanvasRenderingContext2D;
 
 class App {
 
-	macro static function getColors( path : String, start : Int, end : Int ) {
-		var colors = new Array<String>();
-		for( i in start...end+1 ) {
-			var data = haxe.Json.parse( sys.io.File.getContent( '$path/$i.json' ) );
-			var c = data.color;
-			var rgba = om.color.space.RGB.create( c.r, c.g, c.b );
-			colors.push( rgba.toHex() );
+	macro static function getColors( path : String, start : Int, end : Int, split : Int ) {
+		var total = end - start;
+		var parts = new Array<Array<String>>();
+		var i = 0;
+		while( true ) {
+			var j = 0;
+			var part = new Array<String>();
+			parts.push( part );
+			while( j < split ) {
+				var k = start+i;
+				var data = haxe.Json.parse( sys.io.File.getContent( '$path/'+k+'.json' ) ).color;
+				if( data == null ) {
+					trace( 'NULL! [$k]' );
+					data = { r : 0, g : 0, b : 0, a : 1.0 };
+				}
+				var rgb = om.color.space.RGB.create( data.r, data.g, data.b );
+				part.push( rgb.toHex() );
+				j++;
+				if( ++i > total )
+					return macro $v{parts};
+			}
 		}
-		return macro $v{colors};
+		return macro null;
 	}
 
 	#if js
 
-	static var colors = getColors( '/home/tong/dev/pro/archillect/meta', 1, 4000 );
+	static inline var INDEX_START = 1;
+	static inline var INDEX_END = 199795;
+
+	static var numColors = INDEX_END - INDEX_START;
 	static var info : Element;
 
 	static function updatePositionInfo() {
-		info.textContent = (colors.length - window.scrollY)+'-'+(colors.length - (window.scrollY+window.innerHeight)+1);
+		var total = numColors;
+		var start = (total - window.scrollY);
+		var end = (total - (window.scrollY+window.innerHeight));
+		if( end < 1 ) end = 1;
+		info.textContent = start+'-'+end;
 	}
 
 	static function main() {
 
-		var colorsPerCanvas = 1000;
-		var canvas : CanvasElement = null;
-		var ctx : CanvasRenderingContext2D = null;
-		var py = 0;
-		for( i in 0...colors.length ) {
-			if( i == 0 || i % colorsPerCanvas == 0 ) {
-				canvas = document.createCanvasElement();
-				canvas.setAttribute( 'data-index', Std.string(i) );
-				canvas.width = window.innerWidth;
-				canvas.height = colorsPerCanvas;
-				document.body.appendChild( canvas );
-				ctx = canvas.getContext2d();
-				py = 0;
-			}
-			var c = colors[colors.length-i];
-			ctx.fillStyle = c;
-			ctx.fillRect( 0, py, canvas.width, 1 );
-			py++;
-		}
+		var colorBlocks = getColors( '/home/tong/dev/pro/archillect/meta', 1, 199795, 1000 );
 
 		info = document.createDivElement();
 		info.id = 'info';
 		document.body.appendChild( info );
+
+		for( i in 0...colorBlocks.length ) {
+			var colors = colorBlocks[i];
+			var canvas = document.createCanvasElement();
+			canvas.width = window.innerWidth;
+			canvas.height = colors.length;
+			document.body.appendChild( canvas );
+			var ctx = canvas.getContext2d();
+			for( j in 0...colors.length ) {
+				var c = colors[j];
+				ctx.fillStyle = c;
+				ctx.fillRect( 0, j, canvas.width, 1 );
+			}
+		}
 
 		updatePositionInfo();
 
